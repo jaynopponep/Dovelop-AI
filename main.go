826455bd3c6c0898ebf111bab849340f2ef796c6
jaynopponep <noppone.p@gmail.com>
2024-08-11 11:41:26 -0400

    deletion available, creating new task fixed for determining new Task # after deletion

--- main.go
+++ main.go
@@ -48,8 +48,13 @@ func processChoice(choice int) {
 	case 1:
 		fmt.Println("You chose to create a new task")
 		var tasks = getData()
-		var count = len(tasks)
-		var num = count + 1
+		var num float64
+		if len(tasks) <= 0 {
+			num = 1
+		} else {
+			var last = len(tasks) - 1
+			num = float64(int(tasks[last][0].(float64))) + 1
+		}
 		var task string
 		reader := bufio.NewReader(os.Stdin)
 		fmt.Print("Enter your task: ")
@@ -60,13 +65,23 @@ func processChoice(choice int) {
 		}
 		task = strings.TrimSpace(task)
 		// remove newline from input aka give the string a haircut from leading and trailing white space
-		t := Task{num, task}
+		t := Task{int(num), task}
 		createTask(t)
 
 	case 2:
-		fmt.Println("You chose option 2")
+		fmt.Println("You chose to mark a task done")
 	case 3:
-		fmt.Println("You chose option 3")
+		fmt.Println("You chose to delete a task")
+		fmt.Print("Enter the task number you want to delete: ")
+		var choice int
+		_, inputErr := fmt.Scanln(&choice)
+		if inputErr != nil {
+			fmt.Println("Invalid input, try again")
+			return
+		} else {
+			deleteTask(choice)
+		}
+		buildTable()
 	}
 }
 
@@ -89,6 +104,7 @@ func createTask(t Task) {
 	}
 
 	log.Println("Data added")
+	buildTable()
 }
 
 func getData() [][]interface{} {
@@ -97,7 +113,12 @@ func getData() [][]interface{} {
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer file.Close()
+	defer func(file *os.File) {
+		err := file.Close()
+		if err != nil {
+			log.Fatal("Error closing file:", err)
+		}
+	}(file)
 
 	// retrieve tasks in data.json
 	var tasks [][]interface{}
@@ -115,10 +136,37 @@ func getData() [][]interface{} {
 	return tasks
 }
 
+func deleteTask(num int) {
+	tasks := getData()
+	var del int
+	found := false
+	for i, task := range tasks {
+		if int(task[0].(float64)) == num {
+			del = i
+			found = true
+			break
+		}
+	}
+	if found {
+		tasks = append(tasks[:del], tasks[del+1:]...)
+
+		updatedData, err := json.Marshal(tasks)
+		if err != nil {
+			log.Fatal("Error marshalling data:", err)
+		}
+		if err := os.WriteFile("data.json", updatedData, 0644); err != nil {
+			log.Fatal("Error writing new data to file:", err)
+		}
+		fmt.Printf("Deleted task #%d\n", num)
+	} else {
+		fmt.Printf("Task #%d not found\n", num)
+	}
+}
+
 func main() {
 	buildTable()
 	// begin main menu process
-	fmt.Println("Choose an option")
+	fmt.Println("Choose an option" + "\n" + "1. Create a new task" + "\n" + "2. Mark a task done" + "\n" + "3. Delete a task" + "\n" + "4. Edit a task" + "\n" + "5. Exit")
 	var choice int
 	_, inputErr := fmt.Scanln(&choice)
 	if inputErr != nil {
